74206802bd87cc15f49beca860d6e0fd8f260fce
Patrick Valsecchi <patrick.valsecchi@camptocamp.com>
2016-05-29 08:31:26 +0200

    Better test

--- docker/server/tests/test.sh
+++ docker/server/tests/test.sh
@@ -1,15 +1,28 @@
 #!/bin/bash
 set -e
 
+function checkUrl {
+  local url=$1
+  local content=`curl -s $url`
+  if echo "$content" | grep ServiceExceptionReport
+  then
+    echo "Error with $url:"
+    echo "$content"
+    exit 1
+  else
+    echo "Success with $url"
+  fi
+}
+
 docker stop server || true
 docker rm server || true
 
 #docker build -t qgis-server ..
 docker run -d -p 8380:80 --volume=$PWD:/project --name server pvalsecc/qgis-server:latest
 
-URL=http://localhost:8380/
+BASE_URL=http://localhost:8380/
 
-curl "$URL?SERVICE=WMS&REQUEST=GetCapabilities" > /dev/null
-curl "$URL?SERVICE=WMS&VERSION=1.3.0&REQUEST=GetMap&BBOX=-0.69003700000000001,-1.54244000000000003,1.30996000000000001,1.03689999999999993&CRS=EPSG:4326&WIDTH=667&HEIGHT=518&LAYERS=Test&STYLES=&FORMAT=image/jpeg&DPI=96&MAP_RESOLUTION=96&FORMAT_OPTIONS=dpi:96" > /dev/null
+checkUrl "$BASE_URL?SERVICE=WMS&REQUEST=GetCapabilities"
+checkUrl "$BASE_URL?SERVICE=WMS&VERSION=1.3.0&REQUEST=GetMap&BBOX=-0.69003700000000001,-1.54244000000000003,1.30996000000000001,1.03689999999999993&CRS=EPSG:4326&WIDTH=667&HEIGHT=518&LAYERS=Test&STYLES=&FORMAT=image/jpeg&DPI=96&MAP_RESOLUTION=96&FORMAT_OPTIONS=dpi:96"
 
 docker stop server

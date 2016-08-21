8530facd9ca4aa9d7b343ac3835489ca292a20cd
Patrick Valsecchi <patrick.valsecchi@camptocamp.com>
2016-08-21 14:35:53 +0200

    Newer rancher version

--- docker/rocker.sh
+++ docker/rocker.sh
@@ -2,7 +2,7 @@
 #Small wrapper for rocker that downloads it if missing
 
 DIR=`dirname $0`
-DOCKER_URL=https://github.com/grammarly/rocker/releases/download/1.2.0/rocker-1.2.0-linux_amd64.tar.gz
+DOCKER_URL=https://github.com/grammarly/rocker/releases/download/1.3.0/rocker_linux_amd64.tar.gz
 
 if [ ! -f $DIR/tmp/rocker ]
 then
